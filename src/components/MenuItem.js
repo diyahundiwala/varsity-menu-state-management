cb11001ad65b855ac139149cc6f44b3fc2addddc
Diya Hundiwala <diyahundiwala@gmail.com>
2024-01-26 19:01:02 -0500

    doneeeee

---
+++ src/components/MenuItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const MenuItem = props => {
+  const [selected, setSelected] = React.useState(false)
+
+  const toggleSelected = () => {
+    if(!selected) {
+      props.updatePurchaseItems({
+        name: props.name,
+        price: props.price,
+    })
+  } else {
+    props.removePurchasedItem(props.name)
+    }
+    setSelected(!selected)
+  }
+
+  return (
+    <button 
+      class={selected ? 'menu-item selected' : 'menu-item'}
+      onClick={toggleSelected}
+    >
+    <img src={props.picture} alt={props.name} />
+    <dt>{props.name}</dt>
+    <dd>{props.price}</dd>
+    </button>
+  )
+}
+  
+
+export default MenuItem
